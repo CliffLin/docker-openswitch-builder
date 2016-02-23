5a53e1a15b07346ad69671d2b9c60197abf32d3f
CliffLin <cliff820119@gmail.com>
2016-02-23 21:20:00 +0800

    fix path error

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo $0
 if [ $# -ne 2 ];then
-  echo "docker run -tid -v$pwd/:/tmp/ops-builder/image/ ops-builder {OPS_VERSION} {PLATFORM}"
+  echo "docker run -tid -v$pwd/:/tmp/images/ ops-builder {OPS_VERSION} {PLATFORM}"
 fi
 echo 'OPS_VERSION: '$1 >> ansible/group_vars/openswitch
 echo 'PLATFORM: '$2 >> ansible/group_vars/openswitch
 cd ansible;ansible-playbook playbook.yml
+cp -r /tmp/ops-builder/images /tmp/images
